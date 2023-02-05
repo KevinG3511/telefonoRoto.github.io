4bf89afff6ff7e79ed5e056473c2f65e0d6d0a54
Kevin <kevin.guevarevara@cumplimos.co>
2023-02-05 12:22:25 -0500

    aclaracion de las rutas con breve modificacion

--- api/routes/servicio_1.py
+++ api/routes/servicio_1.py
@@ -1,17 +1,27 @@
 from fastapi import APIRouter
+import requests
 servicio1= APIRouter()
 
 @servicio1.get('/')
 async def root():
     'Pagina de inicio'
-    return '<h1>Rutas<h1><br><a href=#>-----<a>'
+    return 'http://127.0.0.1:8000/api1/get_data'
 
 @servicio1.get('/show_data')
 async def root1():
-    """Mostrar informacion"""
+    """Mostrar informacion (lo que recibe request antes del cambio)"""
     results = {
         'name':'Kevin Guevara',
         'age':19
+    }
+    obj = {"status": 200, "results": results, "msg": "Peticion correcta"}
+    return obj
+
+@servicio1.get('/get_data')
+async def root2():
+    """Retornar informacion ('despues del cambio')"""
+    results = {
+        'name':'Kevin Guevara',
     }
     obj = {"status": 200, "results": results, "msg": "Peticion correcta"}
     return obj
