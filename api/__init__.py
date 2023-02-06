3ecb259e3e6f279ad0bae40c5830b6c39c89d2d4
Kevin <kevin.guevarevara@cumplimos.co>
2023-02-05 19:09:22 -0500

    modificacion de url de diferente api

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,33 @@
 from fastapi import APIRouter
-from .routes import servicio_1
+import requests
+# import urllib3
+# import json
 
-router = APIRouter()
+router= APIRouter()
 
-router.include_router(servicio_1.servicio1, prefix='/api1')
+@router.get('/')
+async def root():
+    'Pagina de inicio'
+    return 'http://127.0.0.1:8000/show_data'
+
+@router.get('/show_data')
+async def root1():
+    """Mostrar informacion (lo que recibe request antes del cambio)"""
+    results = {
+        'telefono':'3',
+        'author':'Kevin Guevara'
+    }
+    obj = {"status": 200, "results": results, "msg": "Peticion correcta"}
+    return obj
+
+@router.get("/telefono3")
+def read_item():
+    """inicio"""
+    url = 'https://9st22m.deta.dev/telefono1/holaa'
+    # url = 'https://ojyvp2.deta.dev/telefono4/'+cambios
+    resp = requests.get(url)
+    respuesta = resp.json()
+    return respuesta
 
 if __name__ == '__main__':
     router.run(port=8000)
--- api/routes/servicio_1.py
+++ api/routes/servicio_1.py
@@ -1,28 +1,34 @@
-from fastapi import APIRouter
-import requests
-servicio1= APIRouter()
+# from fastapi import APIRouter
+# import requests
+# servicio1= APIRouter()
 
-@servicio1.get('/')
-async def root():
-    'Pagina de inicio'
-    return 'http://127.0.0.1:8000/api1/get_data'
+# @servicio1.get('/')
+# async def root():
+#     'Pagina de inicio'
+#     return 'http://127.0.0.1:8000/api1/get_data'
 
-@servicio1.get('/show_data')
-async def root1():
-    """Mostrar informacion (lo que recibe request antes del cambio)"""
-    results = {
-        'name':'Kevin Guevara',
-        'age':19
-    }
-    obj = {"status": 200, "results": results, "msg": "Peticion correcta"}
-    return obj
+# @servicio1.get('/show_data')
+# async def root1():
+#     """Mostrar informacion (lo que recibe request antes del cambio)"""
+#     results = {
+#         'name':'Kevin Guevara',
+#         'age':19
+#     }
+#     obj = {"status": 200, "results": results, "msg": "Peticion correcta"}
+#     return obj
 
-@servicio1.get('/get_data')
-async def root2():
-    """Retornar informacion ('despues del cambio')"""
-    results = {
-        'name':'Este es un nuevo mensaje',
-    }
-    obj = {"status": 200, "results": results, "msg": "Peticion correcta"}
-    return obj
-
+# @servicio1.get("/telefono3/{palabra}")
+# def read_item(palabra: str):
+#     """inicio"""
+#     cambios = palabra.replace('A', '4').replace('E', '3').replace(
+#         'I', '1').replace('O', '0').replace('U', '8')
+#     url = 'https://ojyvp2.deta.dev/telefono4/'+cambios
+#     resp = requests.get(url)
+#     respuesta = resp.json()
+#     return {'status': 200,
+#             'results': {
+#                 'change1': palabra,
+#                 'change2': cambios,
+#                 'change': respuesta
+#             }
+#             }
